2922dda1ac3765044f7413bfffbf449fd24a7651
terranrun <45706959+terranrun@users.noreply.github.com>
2022-02-18 22:39:59 +0500

    `

---
+++ Assets/Scripts/CharacterAnimationEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationEvents : MonoBehaviour
+{
+    Character character;
+    public Character pers;
+
+    void Start()
+    {
+        character = GetComponentInParent<Character>();
+    }
+
+    void ShootEnd()
+    {
+        character.SetState(Character.State.Idle);
+    }
+
+    void AttackEnd()
+    {
+        character.SetState(Character.State.RunningFromEnemy);
+        
+    }
+    void Deadpers()
+    {
+        pers.SetState(Character.State.Dead);
+    }
+
+}
